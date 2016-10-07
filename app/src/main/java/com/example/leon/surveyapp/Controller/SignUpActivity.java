67aa3b6783c28ccfab856e3ccb1a04475f9c4036
Leon <gradisar.leon@gmail.com>
2016-10-07 11:44:58 +0200

    Added UI for Sign In and Sign Up

---
+++ app/src/main/java/com/example/leon/surveyapp/Controller/SignUpActivity.java
@@ -0,0 +1,22 @@
+package com.example.leon.surveyapp.Controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.leon.surveyapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void cent(View view) {
+        Intent intent = new Intent(this, CentActivity.class);
+        startActivity(intent);
+    }
+}
